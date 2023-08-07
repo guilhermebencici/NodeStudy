5d87821825152692a85494630c508a9373fd3d69
GuilhermeSouza <guilhermebencici@gmail.com>
2023-08-07 08:42:28 -0300

    NPM fundamental: Script

---
+++ 4_NPM_FUNDAMENTAL/5_scripts/index.js
@@ -0,0 +1,11 @@
+//Ao utilizarmos a flag: --save-dev estamos separando ele no package.json dos demais
+
+const _ = require('lodash')
+const chalk = require('chalk')
+
+const a = [1, 2, 3, 4, 5]
+const b = [2, 4, 6, 7, 8]
+
+const diff = _.difference(a, b)
+
+console.log(chalk.red.bold(diff))
